e37f22ba2a93b572af05af7fd3b99b3dd972d1ba
jodrasko <youremail@provider.com>
2024-12-05 11:39:18 -0800

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/price-calc-react-tailwind"
+  plugins: [react()]
 });
