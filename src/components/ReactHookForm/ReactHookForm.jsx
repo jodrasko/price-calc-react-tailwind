c4f502931b8e7010e353c58ddd4695b316d3a71f
jodrasko <youremail@provider.com>
2024-12-10 12:17:00 -0800

    Added comments for clarity, updated Tailwind CSS styles, cleaned up code

--- src/components/ReactHookForm/ReactHookForm.jsx
+++ src/components/ReactHookForm/ReactHookForm.jsx
@@ -37,113 +37,143 @@ const ReactHookForm = () => {
   };
 
   return (
-    <div className="flex justify-evenly min-h-screen px-7 py-7">
-      <div className="shadow-2xl pt-6 bg-sky-200 border-2 border-solid border-pink-400 rounded">
-        <h1 className="font-serif font-black text-lg md:text-2xl px-20 py-6">
+    <div className="flex items-center justify-center p-3">
+      <div className="w-full max-w-md mx-auto  bg-sky-200 border-2 border-solid border-pink-400 shadow-2xl rounded-2xl overflow-hidden">
+        <h1 className="font-serif font-bold text-center text-2xl p-4">
           Price Calculator
         </h1>
 
-        <form onSubmit={handleSubmit(onSubmit)} noValidate>
-          <div className="flex-col justify-between space-y-4 font-mono py-3 px-7 lg:max-xl:px-8 row-auto">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+          className="p-4 space-y-4"
+        >
+          <div className="space-y-4">
+            {/* Original Price Input */}
             <div>
-              <h2 className="block font-mono text-sm md:text-lg font-semibold text-black ml-4 mb-2">
+              <label
+                htmlFor="price"
+                className="block font-mono text-sm md:max-2xl:text-base font-semibold text-black ml-1 mb-2"
+              >
                 Original Price ($)
-              </h2>
+              </label>
+
               <input
-                className={
-                  errors.price
-                    ? "min-w-60 text-xs md:max-xl:text-base mx-3.5 px-3 py-2 pr-10 border-2 border-red-500 focus:outline-none focus:border-red-700 rounded-lg mb-2"
-                    : "min-w-60 text-xs md:max-xl:text-base ml-3.5 px-3 py-2 pr-10 border border-blue-500  focus:outline-none focus:border-blue-800 focus:ring-1 focus:ring-blue-800 rounded-lg mb-2"
-                }
+                className={`w-full border rounded-lg transition-all 
+                  ${
+                    errors.price
+                      ? "text-xs md:max-2xl:text-base px-2 py-1 border-2 border-red-500 focus:outline-none focus:border-red-700 rounded-lg mb-2"
+                      : "text-xs md:max-2xl:text-base px-2 py-1 border border-blue-500  focus:outline-none focus:border-blue-800 focus:ring-1 focus:ring-blue-800 rounded-lg mb-2"
+                  }`}
                 id="price"
                 type="text"
-                placeholder="enter original price"
+                placeholder="Enter original price"
                 {...register("price", {
                   required: "required",
                   pattern: {
                     value: /^[$€£]?(\d{1,3}(,\d{3})*|\d+)(\.\d{2})?$/,
-                    message: "Invalid price value entered."
+                    message: "Invalid price format (e.g., $100 or 100.50)"
                   }
                 })}
               />
               {errors.price && (
-                <span className="md:max-xl:block mb-2 mt-2 ml-4 text-red-500 text-sm md:text-base lg:max-xl:text-base">
+                <span className="md:max-2xl:block mb-2 ml-2 text-red-500 text-sm md:text-base lg:max-xl:text-base">
                   {errors.price.message}
                 </span>
               )}
             </div>
-            <div>
-              <h2 className="block font-mono  text-sm md:text-lg font-semibold text-black ml-4 mb-2">
-                Sales Discount (%)
-              </h2>
-              <input
-                className={
-                  errors.discount
-                    ? "min-w-60 text-xs mx-3.5 md:max-xl:text-base px-4 py-2 pr-10 border-2 border-red-500 focus:outline-none focus:border-red-700 rounded-lg mb-2"
-                    : "min-w-60 text-xs md:max-xl:text-base ml-3.5 px-3 py-2 pr-10 border border-blue-500  focus:outline-none focus:border-blue-800 focus:ring-1 focus:ring-blue-800  rounded-lg mb-2"
-                }
-                id="discount"
-                type="text"
-                placeholder="enter sales discount"
-                {...register("discount", {
-                  required: "required",
-                  pattern: {
-                    value: /^(\d{1,2})$/,
-                    message: "Invalid sales discount value entered."
-                  }
-                })}
-              />
-              {errors.discount && (
-                <span className="md:max-xl:block mb-2 mt-2 ml-4 text-red-500 text-sm md:text-base lg:max-xl:text-base">
-                  {errors.discount.message}
-                </span>
-              )}
+
+            {/* Discount Input */}
+            <div className="space-y-4">
+              <div>
+                <label
+                  htmlFor="discount"
+                  className="block font-mono text-sm md:max-2xl:text-base font-semibold text-black ml-1 mb-2"
+                >
+                  Sales Discount (%)
+                </label>
+                <input
+                  className={`
+                  w-full px-3 py-2 border rounded-lg transition-all
+                  ${
+                    errors.discount
+                      ? "text-xs md:max-2xl:text-base px-2 py-1 border-2 border-red-500 focus:outline-none focus:border-red-700 rounded-lg mb-2"
+                      : "text-xs md:max-2xl:text-base px-2 py-1 border border-blue-500  focus:outline-none focus:border-blue-800 focus:ring-1 focus:ring-blue-800  rounded-lg mb-2"
+                  }`}
+                  id="discount"
+                  type="text"
+                  placeholder="Enter sales discount"
+                  {...register("discount", {
+                    required: "required",
+                    pattern: {
+                      value: /^(\d{1,2})$/,
+                      message: "Invalid sales discount value entered."
+                    }
+                  })}
+                />
+                {errors.discount && (
+                  <span className="md:max-2xl:block mb-2 ml-2 text-red-500 text-sm md:text-base lg:max-xl:text-base">
+                    {errors.discount.message}
+                  </span>
+                )}
+              </div>
             </div>
-            <div>
-              <h2 className="block font-mono  text-sm md:text-lg font-semibold text-black ml-4 mb-2">
-                Total Sales Tax (%)
-              </h2>
-              <input
-                className={
-                  errors.tax
-                    ? "min-w-60 text-xs mx-3.5 md:max-xl:text-base px-4 py-2 pr-10 border-2  border-red-500 focus:outline-none focus:border-red-700 rounded-lg mb-2"
-                    : "min-w-60 text-xs md:max-xl:text-base ml-3.5 px-3 py-2 pr-10 border border-blue-500  focus:outline-none focus:border-blue-800 focus:ring-1 focus:ring-blue-800  rounded-lg mb-2"
-                }
-                id="tax"
-                type="text"
-                placeholder="enter total sales tax"
-                {...register("tax", {
-                  required: "required",
-                  pattern: {
-                    value: /^[$€£]?(\d{1,3}(,\d{3})*|\d+)(\.\d{2})?$/,
-                    message: "Invalid tax value entered."
-                  }
-                })}
-              />
-              {errors.tax && (
-                <span className="md:max-xl:block mb-2 mt-2 ml-4 text-red-500  text-sm md:text-base lg:max-xl:text-base">
-                  {errors.tax.message}
-                </span>
-              )}
+
+            {/* Tax Input */}
+            <div className="space-y-4">
+              <div>
+                <label
+                  htmlFor="tax"
+                  className="block font-mono text-sm md:max-2xl:text-base font-semibold text-black ml-1 mb-2"
+                >
+                  Total Sales Tax (%)
+                </label>
+                <input
+                  className={`
+                  w-full space-y-2 px-3 py-2 border rounded-lg transition-all 
+                  ${
+                    errors.tax
+                      ? "text-xs md:max-2xl:text-base px-2 py-1 border-2  border-red-500 focus:outline-none focus:border-red-700 rounded-lg mb-2"
+                      : "text-xs md:max-2xl:text-base px-2 py-1 border border-blue-500  focus:outline-none focus:border-blue-800 focus:ring-1 focus:ring-blue-800  rounded-lg mb-2"
+                  }`}
+                  id="tax"
+                  type="text"
+                  placeholder="Enter total sales tax"
+                  {...register("tax", {
+                    required: "required",
+                    pattern: {
+                      value: /^[$€£]?(\d{1,3}(,\d{3})*|\d+)(\.\d{2})?$/,
+                      message: "Invalid tax value entered."
+                    }
+                  })}
+                />
+                {errors.tax && (
+                  <span className="md:max-2xl:block mb-2 ml-2 text-red-500 text-sm md:text-base lg:max-xl:text-base">
+                    {errors.tax.message}
+                  </span>
+                )}
+              </div>
             </div>
           </div>
 
-          <div className="mt-6 ml-24 md:max-lg:ml-24">
+          {/* Calculate Button */}
+          <div className="ml-36 md:max-2xl:ml-20 pb-3">
             <button
               type="submit"
-              className="w-30 md:w-40 px-5 py-2 md:px-6 md:py-3 bg-yellow-300 text-sm md:max-xl:text-base font-serif text-black tracking-wide font-semibold rounded-full border border-black hover:bg-yellow-200"
+              className="w-50 md:max-2xl:w-3/4 mt-1 px-5 py-2 md:px-6 md:py-3 bg-yellow-300 text-sm md:max-xl:text-base font-serif text-black tracking-wide font-semibold rounded-full border border-black hover:bg-yellow-200"
             >
               Calculate
             </button>
           </div>
 
+          {/* Result Display */}
           {finalPrice && (
-            <div className="flex-col justify-between font-mono py-3 px-8 pr-14 row-auto mt-6">
-              <h3 className="font-mono text-sm md:text-base lg:text-xl font-semibold text-black px-5 py-2 row-auto">
+            <div className="space-y-2 pb-3">
+              <label className="block font-mono text-sm font-semibold text-black ml-1">
                 Final Sales Price
-              </h3>
-              <div className="min-w-60 text-xs md:max-xl:text-base ml-3.5 px-4 py-2 pr-10 border border-blue-700 bg-white rounded-lg">
-                <h3 className="font-mono text-xs md:max-xl:text-base font-semibold text-black">
+              </label>
+              <div className="text-xs md:max-2xl:text-base px-2 py-1 border border-blue-700 bg-white rounded-lg">
+                <h3 className="font-mono text-xs md:max-2xl:text-base font-semibold text-black">
                   ${finalPrice}
                 </h3>
               </div>
